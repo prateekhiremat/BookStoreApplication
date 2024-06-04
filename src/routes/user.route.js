e813ccd1ae1e47977317b62ba608d69baa69e28e
prateekhiremath <prateek.s.hiremath123@gmail.com>
2024-06-04 23:39:37 +0530

    [Prateek]Add: Admin Registration

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,10 +2,12 @@ import express from 'express';
 import * as userController from '../controllers/user.controller';
 import { userRegisterValidator } from '../validators/user.validator';
 import { userAuth } from '../middlewares/auth.middleware';
-import { userRole } from '../utils/user.role';
+import { adminRole, userRole } from '../utils/user.role';
 
 const router = express.Router();
 
 router.post('', userRegisterValidator, userRole, userController.userRegistration);
+
+router.post('/admin', userRegisterValidator, adminRole, userController.userRegistration);
 
 export default router;
